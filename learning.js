e442a665ab6adcee59c3295e154c064ee77976b2
Nilabh <nilabhaditya6@gmail.com>
2021-11-06 18:23:05 +0530

    commited

--- learning.js
+++ learning.js
@@ -1,3 +1,12 @@
 const http = require("http"); // http varible with require function that has global module inside.
-function rqListener(req, res) {} // function has two arguments here one for request and resond for response.
-http.createServer(rqListener); //   variable with method name createserver.
+/* function rqListener(req, res) {} // function has two arguments here one for request and resond for response.
+
+const server= http.createServer(rqListener); //   variable with method name createserver.
+*/
+// above one is one way to create a server
+const server = http.createServer((req, res) => {
+  console.log(req.url,req.method,req.);
+  process.exit();
+}); // storing the server.
+
+server.listen(3000);
