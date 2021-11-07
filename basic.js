0eea4b8020965c8aacf1a51082e60a9cffebf354
Nilabh <nilabhaditya6@gmail.com>
2021-11-07 12:06:33 +0530

    commitone

--- basic.js
+++ basic.js
@@ -1,1 +1,20 @@
 "use strict";
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassegers = 1,
+  price = 199 * numPassegers
+) {
+  /* numPassegers = numPassegers || 1;
+  price = price || 199;   es 5 assignment*/
+  const booking = {
+    flightNum,
+    numPassegers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking("LH123");
+createBooking("HT134", 2, 334);
+createBooking("hs123", undefined, 1000);
