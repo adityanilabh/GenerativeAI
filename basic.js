ed92bf7492cb95ef0f6593a0ebe1dbde40452c4b
Nilabh <nilabhaditya6@gmail.com>
2021-11-07 13:44:34 +0530

    commiting

--- basic.js
+++ basic.js
@@ -1,12 +1,12 @@
-"use strict";
-const bookings = [];
+"use strict"; /*
+/*const bookings = [];
 const createBooking = function (
   flightNum,
   numPassegers = 1,
   price = 199 * numPassegers
 ) {
   /* numPassegers = numPassegers || 1;
-  price = price || 199;   es 5 assignment*/
+  price = price || 199;   es 5 assignment*/ /*
   const booking = {
     flightNum,
     numPassegers,
@@ -18,3 +18,4 @@ const createBooking = function (
 createBooking("LH123");
 createBooking("HT134", 2, 334);
 createBooking("hs123", undefined, 1000);
+*/
