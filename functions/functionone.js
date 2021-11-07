d2bdbb76753d1c8afb7217d6ca5229d9bec51800
Nilabh <nilabhaditya6@gmail.com>
2021-11-07 18:02:37 +0530

    message

--- functions/functionone.js
+++ functions/functionone.js
@@ -29,3 +29,33 @@ greeterHey("Jonas");
 greeterHey("Steven");
 
 greet("Hello")("Jonas");
+
+// using arrow function
+const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+greetArr("Hi")("Jonas");
+
+const airIndia = {
+  airline: "AirIndia",
+  lataCode: "AI",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.lataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.lataCode}${flightNum}`, name });
+  },
+};
+
+airIndia.book(238, "Jonas Schmedtmann");
+airIndia.book(635, "John Smith");
+const eurowings = {
+  airline: "Eurowings",
+  lataCode: "EW",
+  bookings: [],
+};
+const book = airIndia.book;
+book.call(eurowings, 23, "sarah williams");
+console.log(eurowings);
+book.call(airIndia, 235, "Mary Cooper");
+console.log(airIndia);
