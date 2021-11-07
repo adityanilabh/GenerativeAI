5f01eb451e0bb3c73c1898f9d7b8470dd533ac67
Nilabh <nilabhaditya6@gmail.com>
2021-11-07 14:05:23 +0530

    mergeone

--- basic.js
+++ basic.js
@@ -19,3 +19,32 @@ createBooking("LH123");
 createBooking("HT134", 2, 334);
 createBooking("hs123", undefined, 1000);
 */
+/*
+const flight = "LH234";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 23204243434,
+};
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr. " + passenger.name;
+  if (passenger.passport === 23204243434) {
+    alert("Checked in");
+  } else {
+    alert("Wrong passport");
+  }
+};
+checkIn(flight, jonas);
+*/
+const people = {
+  name: "nilabh",
+  age: 21,
+  enroll: 18104074,
+};
+const changeAge = (peoples) => {
+  peoples.name = "satyam";
+  peoples.age = 25;
+  peoples.enroll = 599999;
+};
+changeAge(people);
+console.log(people);
